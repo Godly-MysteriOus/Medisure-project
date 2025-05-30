5788e679649869140b4ad526cdea7c476a94000d
Godly-MysteriOus <singhrajputjayant8@gmail.com>
2025-05-30 18:58:49 +0530

    home page footer added - responsive Added dynamic rate limiting and refresh time

--- routes/commonRoute.js
+++ routes/commonRoute.js
@@ -9,10 +9,4 @@ router.post('/raise-user-query',
     [checks.emailValidation('emailId'),checks.mobileNumberValidation('mobileNo'),checks.basicMessageValidation('message')],
     rateLimit,csrfProtection,commonController.postUserQueries
 );
-router.get('/a',(req,res,next)=>{
-    res.render('500Page');
-});
-router.get('/b',(req,res,next)=>{
-    res.render('429Page');
-})
 module.exports = router;

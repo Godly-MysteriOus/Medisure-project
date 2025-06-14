ec89188066113c6b9569ec311924bba7adeb9f6e
Godly-MysteriOus <singhrajputjayant8@gmail.com>
2025-06-15 04:53:36 +0530

    email OTP generation and Regeneration logic in place, rateLimit now takes refresh value in hr and min

--- routes/commonRoute.js
+++ routes/commonRoute.js
@@ -5,10 +5,10 @@ const commonController = require('../controllers/commonController');
 const rateLimit = require('../utils/RateLimiter/rateLimiter');
 const checks = require('../utils/check/check');
 const fileConstants = require('../utils/FileConstants');
-
 router.post('/subscribe-to-newsletter',[checks.emailValidation('email')],rateLimit(),csrfProtection,commonController.postSubscriptionToNewsLetter);
 router.post('/raise-user-query',
     [checks.emailValidation('emailId'),checks.mobileNumberValidation('mobileNo'),checks.basicMessageValidation('message')],
     rateLimit(),csrfProtection,commonController.postUserQueries
 );
+router.post('/generate-OTP',[checks.emailValidation('emailId')],rateLimit(3,60),csrfProtection,commonController.postEmailOTPGeneration);
 module.exports = router;

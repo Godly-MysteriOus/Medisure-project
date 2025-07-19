a64fa6cb999a57046b16f8672a50c47ea1662906
Godly-MysteriOus <singhrajputjayant8@gmail.com>
2025-07-20 03:11:10 +0530

    making a call to agent to keep it up and running

--- controllers/agentController/testCron.js
+++ controllers/agentController/testCron.js
@@ -4,8 +4,8 @@ const path = require('path');
 const projectRoot = path.resolve(__dirname, '../');
 const filePathRelativeToRoot = path.relative(projectRoot, __filename);
 const logger = require('../../utils/Logger/logger')(filePathRelativeToRoot);
-
-
+const axios = require('axios');
+const config = require('../../config');
 exports.createTestEntry = async(req,res,next)=>{
     logger.info('Inside createTestEntry method !!!');
     try{
@@ -14,15 +14,27 @@ exports.createTestEntry = async(req,res,next)=>{
         const createEntry = await sharedModule.testCronDB.create({message:'Hi',createDate:date,createTime:time});
         logger.debug('Created test entry successfully');
         if(createEntry){
-            return res.status(200).json({
+            console.log(config.medisureAgentURI+'test-create-result');
+            await axios.post(config.medisureAgentURI+'test-create-result',{
                 success:true,
                 message:'Successfully created the entry'
-            });
+            },{
+                headers:{'Content-Type':'application/json'},
+            }).then(()=>logger.debug('Made a request to agent successfully')).then(()=>res.status(200).json({
+                success:true,
+                message:'Successfully created the entry'
+            })).catch(err=>logger.error(err));
         }else{
-            return res.status(400).json({
+            console.log(config.medisureAgentURI+'test-create-result');
+            await axios.post(config.medisureAgentURI+'test-create-result',{
+                success:true,
+                message:'Successfully created the entry'
+            },{
+                headers:{'Content-Type':'application/json'},
+            }).then(()=>logger.debug('Made a request to agent successfully')).then(()=>res.status(400).json({
                 success:false,
                 message:'Error creating entry',
-            });
+            })).catch(err=>logger.error(err));
         }
     }catch(err){
         logger.error('Error inside createTestEntry method !!!');
